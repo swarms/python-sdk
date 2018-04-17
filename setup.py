1f5a7903bea56b2ed6bd3dd89637d293592162ff
stubbi <jannes.stubbemann@swarms.tech>
2018-04-17 13:12:01 +0200

    release 0.11

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='swarms-sdk',
-    version='0.10',
+    version='0.11',
     description='The Swarms API SDK',
     url='http://github.com/swarms/python-sdk',
     author='Swarms Technologies',
